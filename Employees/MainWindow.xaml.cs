dfa5073d4bc7dc8ab7f8145abac7975d3299117a
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-05-19 17:03:02 +0300

    Add files via upload

---
+++ Employees/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Employees
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StudentRepository studentRepository;
+
+        public ObservableCollection<Student> Students { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            studentRepository = new StudentRepository();
+            Students = new ObservableCollection<Student>(studentRepository.GetStudents());
+            DataContext = this;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = txtSearch.Text;
+            Students = new ObservableCollection<Student>(studentRepository.SearchStudents(searchText));
+            RefreshDataGrid();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            Students = new ObservableCollection<Student>(studentRepository.GetStudents());
+            RefreshDataGrid();
+        }
+
+        private void RefreshDataGrid()
+        {
+            
+            DataGrid.ItemsSource = null;
+            DataGrid.ItemsSource = Students;
+
+        }
+
+    }
+}
---
+++ Employees/StudentRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public class StudentRepository
+    {
+        private string connectionString = "Data Source=MSI;Initial Catalog=Financial_Indicators;Integrated Security=True";
+
+        public List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Students";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Student student = new Student
+                            {
+                                ID = (int)reader["ID"],
+                                FirstName = (string)reader["FirstName"],
+                                LastName = (string)reader["LastName"],
+                                Age = (int)reader["Age"]
+                            };
+                            students.Add(student);
+                        }
+                    }
+                }
+            }
+
+            return students;
+        }
+
+        public List<Student> SearchStudents(string searchText)
+        {
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Students WHERE FirstName LIKE @SearchText OR LastName LIKE @SearchText";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@SearchText", "%" + searchText + "%");
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Student student = new Student
+                            {
+                                ID = (int)reader["ID"],
+                                FirstName = (string)reader["FirstName"],
+                                LastName = (string)reader["LastName"],
+                                Age = (int)reader["Age"]
+                            };
+                            students.Add(student);
+                        }
+                    }
+                }
+            }
+
+            return students;
+        }
+    }
+}
